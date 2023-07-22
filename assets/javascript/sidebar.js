1737f9d9b533b721fa3af3c7ef3d1a47791b6a62
redlue <steven24koning@gmail.com>
2023-07-22 16:43:27 +0200

    better signup style

--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -1,12 +1,20 @@
-function solid(i){
+function solid(i) {
     $(i).toggleClass("fa-regular");
     $(i).toggleClass("fa-solid");
 }
 
 function srcswitch(image) {
-    image.setAttribute('src', 'assets/imgs/instaweb-inverted.png');
+    image.setAttribute("src", "assets/imgs/instaweb-inverted.png");
 }
 
 function srcunswitch(image) {
-    image.setAttribute('src', 'assets/imgs/instaweb-lines.png');
+    image.setAttribute("src", "assets/imgs/instaweb-lines.png");
+}
+
+function title(title) {
+    $(title).toggleClass("visible-title");
+}
+
+function roll(i) {
+    $(i).toggleClass("rotate");
 }
